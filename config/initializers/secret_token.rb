49dac79f704f55fea5a199b252c582fc1e593a4b
Deepak N <endeep123@gmail.com>
2011-09-03 16:31:36 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neo4jObserversExample::Application.config.secret_token = '6731d10a1eeccbdb91efa87176a0eadbc960512009535040868a69104423c3fd3bd9497deb57c5fc835e724609e6f106ea799defd0837e39e374c83b0ab9d3ac'
